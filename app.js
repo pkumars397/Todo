ffe81caef8145a092c0ada34a486baa961c60469
Prashant Kumar <pkumars397@gmail.com>
2023-01-22 09:42:29 +0530

    added styles

--- app.js
+++ app.js
@@ -2,38 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   var today = new Date();
   var currentDay = today.getDay();
-  var day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thrusday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("currentday is not in range.");
-  }
-  res.render("list", { kindofday: day });
+  var options = {
+    weekday: "long",
+    year: "numeric",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindofday: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
