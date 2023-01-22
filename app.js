e755621c03241ce9f02278d2811c53e824ff5cbc
Prashant Kumar <pkumars397@gmail.com>
2023-01-22 15:09:03 +0530

    upadated

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   var today = new Date();
   var currentDay = today.getDay();
